4849ec420b762acfb1747a14af9bc3dcc8533687
AstridR <astrid.meyer@yahoo.de>
2020-07-14 10:29:30 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
-## The following functions only work together, as cacheSolve requires to get a value from makeCacheMatrix
+## The following functions only work together, as cacheSolve requires to get a value from makeCacheMatrix. Because objects are cached 
+## and a list is returned by makeCachematrix, objects can be accessed outside of the function environments.
 
-# The first function makeCacheMatrix consists of setters and getters for x and minv, a matrix and its inverse
+# The first function makeCacheMatrix consists of setters and getters for x and inv, a matrix and its inverse
 # Since a list is returned, objects can be accessed by : aMatrix <- makeCacheMatrix(typeinmatrix) and then: aMatrix$get(...), aMatrix$set(...)
-
 
 # example matrix: x <- matrix(data = c(2,1,0,-1,2,-1,0,-2,1), 3,3)
 
 makeCacheMatrix <- function(x = matrix()) {       # x is defined as empty vector, no further definition necessary
-        inv <- NULL                               # m is defined as a NULL vector for later use of mean calculation 
+        inv <- NULL                               # inv is defined as a NULL vector for later use of inverse calculation 
         
         set <- function(y) {                      
                 x <<- y                           # the value of the input argument y is assigned to x 
-                inv <<- NULL                      # m is set to NULL again in case m already has a value in makeVector
+                inv <<- NULL                      # inv is set to NULL again in case inv already has a value in makeCachematrix
         }
         
         get <- function() x                    # getter function: get retrieves the value for x, which is not defined in get() and
-                                               # therefore taken from the makeVector environment
-        setinverse <- function(solve) inv <<- solve   # setter function for the mean. m needs to be taken from the parent environment by <<-
+                                               # therefore taken from the makeCacheMatrix environment
+        setinverse <- function(solve) inv <<- solve   # setter function for the inverse inv needs to be taken from the parent environment by <<-
         
-        getinverse <- function() inv                # m is taken from the parent environment again, so after setmean has been calculated
+        getinverse <- function() inv                # inv is taken from the parent environment again, so after setinverse has been calculated
         
         list(set = set, get = get,             # creates a list of both values and functions
              setinverse = setinverse,
@@ -29,13 +29,13 @@ makeCacheMatrix <- function(x = matrix()) {       # x is defined as empty vector
 ## cacheSolve only works with makeCacheMatrix. it takes the value by getinverse(), returns the inverse or calculates it from x (by get())
 
 cacheSolve <- function(x, ...) {
-        inv <- x$getinverse()                # the function tries to get a value from x which was calculated in makeVector and assign it to m
+        inv <- x$getinverse()                # the function tries to get a value from inv from makeCacheMatrix 
         
-        if(!is.null(inv)) {                  # if x is not NULL it is returned to the parent environment
+        if(!is.null(inv)) {                  # if x is not NULL, it is returned (to the parent environment)
                 message("getting cached data")
                 return(inv)
         }
-        data <- x$get()                      # only if m is NULL, then the function gets  x from get() in makevector and calculates a mean
+        data <- x$get()                      # only if inv is NULL, then the function gets x from get() in makeCacheMatrix and calculates the inverse
         inv <- solve(data, ...)
         x$setinverse(inv)
         inv
